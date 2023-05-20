12c6465bb92de01578e200d11cadb81ba7d7b50d
spectrum007 <kodopav134@fanneat.com>
2023-05-20 13:08:51 -0300

    first commit

---
+++ web/src/components/EmptyMemory.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function EmptyMemory() {
+  return (
+    <div className="flex flex-1 items-center justify-center p-16">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <Link className="underline hover:text-gray-50" href="/memories/new">
+          criar agora!
+        </Link>
+      </p>
+    </div>
+  )
+}
---
+++ web/src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import Image from 'next/image'
+
+import { getUser } from '@/lib/auth'
+
+export default function Profile() {
+  const { name, avatarUrl } = getUser()
+
+  return (
+    <div className="flex items-center gap-3 text-left">
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
+
+      <p className="max-w-[140px] text-sm leading-snug">
+        {name}
+        <a
+          className="block text-red-400 hover:text-red-300"
+          href="/api/auth/logout"
+        >
+          Quero sair
+        </a>
+      </p>
+    </div>
+  )
+}
